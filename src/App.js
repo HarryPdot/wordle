516ea16a7a082d66aa7a0bfc6124ff2825d1c344
Harry <harrypham_@hotmail.com>
2023-02-02 17:04:59 +1100

    add skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">wordle</h1>
+      <div className="wordle-blocks">
+        <div className="word-blocks">
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
+        <div className="word-blocks">
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
+        <div className="word-blocks">
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
+        <div className="word-blocks">
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
+        <div className="word-blocks">
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
+        <div className="word-blocks">
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+          <div className="block"></div>
+        </div>
+      </div>
+      <div className="keyboard">
+        <div className="keyboard-row-1">
+          <div className="key">Q</div>
+          <div className="key">W</div>
+          <div className="key">E</div>
+          <div className="key">R</div>
+          <div className="key">T</div>
+          <div className="key">Y</div>
+          <div className="key">U</div>
+          <div className="key">I</div>
+          <div className="key">O</div>
+          <div className="key">P</div>
+        </div>
+        <div className="keyboard-row-2">
+          <div className="key">A</div>
+          <div className="key">S</div>
+          <div className="key">D</div>
+          <div className="key">F</div>
+          <div className="key">G</div>
+          <div className="key">H</div>
+          <div className="key">J</div>
+          <div className="key">K</div>
+          <div className="key">L</div>
+        </div>
+        <div className="keyboard-row-3">
+          <div className="key key-enter">ENTER</div>
+          <div className="key">Z</div>
+          <div className="key">X</div>
+          <div className="key">C</div>
+          <div className="key">V</div>
+          <div className="key">B</div>
+          <div className="key">N</div>
+          <div className="key">M</div>
+          <div className="key key-delete">DELETE</div>
+        </div>
+      </div>
     </div>
   );
 }
